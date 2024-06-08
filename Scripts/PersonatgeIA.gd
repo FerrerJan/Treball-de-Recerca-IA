538e105c32f46954b56e6e8d60aaaf402f8d810f
Marc Pérez Fusco <07mapefu@gmail.com>
2024-06-08 22:55:14 +0200

    NouScriptFeedForward
    Ara és recursiu i funciona per qualsevol xarxa neuronal

--- Scripts/PersonatgeIA.gd
+++ Scripts/PersonatgeIA.gd
@@ -100,6 +100,12 @@ func feedforward(inputs):
 	var outputs = []
 	for _i in range(num_inputs):
 		neurons[_i].output = inputs[_i]
+	for _i in range(num_outputs):
+		valor(_i + num_inputs + num_hidden)
+		outputs.append(neurons[_i + num_inputs + num_hidden].output)
+	'''
+	for _i in range(num_inputs):
+		neurons[_i].output = inputs[_i]
 	var value :float = 0.0
 	for _j in range(num_hidden):
 		value = 0
@@ -117,11 +123,20 @@ func feedforward(inputs):
 	for _i in range(num_outputs):
 		outputs.append(neurons[num_inputs + num_hidden + _i].output)
 	#print(outputs)
+	'''
 	return outputs
 
 func sigmoid(x):
 	return 1 / (1 + exp(-x))
 
+func valor(n):
+	var output :float = 0.0
+	for conection in connections:
+		if conection.to_neuron == n and neurons[conection.from_neuron].output != 0:
+			output += neurons[conection.from_neuron].output * conection.weight
+		elif conection.to_neuron == n and neurons[conection.from_neuron].output == 0:
+			output += valor(conection.from_neuron)
+	neurons[n].output = output
 
 func _on_area_2d_body_entered(body):
 	mort = true
