f61b5fe32f8b0ca5371b1f7b2ab4d6b401b92b43
Teo Clerici <clerici.teo5@gmail.com>
2024-06-09 17:25:41 +0200

    reposicionament de codi y comentaris, NO HI HA CAP MODIFICACIÓ EN EL CODI

--- Scripts/PersonatgeIA.gd
+++ Scripts/PersonatgeIA.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
-
+# Constantes
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0
 var animacio = 0
 
+# Constantes de gravedad y velocidad
 var gravity = (ProjectSettings.get_setting("physics/2d/default_gravity"))/1.4
 
-
+# Variables para la red neuronal artificial
 var neurons = []
 var connections = []
 var fitness := 0.0
@@ -17,17 +18,35 @@ var num_outputs = 1
 var mort := false
 var life := true
 
+#Classes necesarias para la red neuronal
+class NEATNeuron:
+	var output = 0.0
+
+	func __init__():
+		pass
+
+class NEATConnection:
+	var from_neuron = 0
+	var to_neuron = 0
+	var weight = 0.0
+
+	func __init__():
+		pass
 
 func _ready():
+	# Inicialización de la escena
 	$CollisionShape2D.disabled = false
+	# Creación de neuronas de entrada, ocultas y salida
 	for _i in range(num_inputs):  # 3 neuronas de entrada
 		neurons.append(NEATNeuron.new())
 	for _i in range(num_hidden):  # 2 neuronas ocultas
 		neurons.append(NEATNeuron.new())
 	for _i in range(num_outputs):  # 1 neurona de salida
 		neurons.append(NEATNeuron.new())
+	# Creación de conexiones entre neuronas
 	for _i in range(num_inputs * num_hidden + num_hidden * num_outputs):  # Conexiones entre neuronas
 		connections.append(NEATConnection.new())
+	# Inicialización de pesos aleatorios
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	var index := 0
@@ -45,60 +64,60 @@ func _ready():
 			index += 1
 
 func _physics_process(delta):
+	# Verificar si el pájaro está muerto
+	
 	if mort == false:
-		fitness = Global.distancia
-		if not is_on_floor():
-			velocity.y += gravity * delta
-		if feedforward(inputs())[0] > 0.6:
-			velocity.y = JUMP_VELOCITY
-		if life == true:
+		# Actualizar la fitness        
+		fitness = Global.distancia        
+		# Aplicar gravedad si no está en el suelo        
+		if not is_on_floor():            
+			velocity.y += gravity * delta        
+		# Saltar si la salida de la red neuronal es mayor que 0.6        
+		if feedforward(inputs())[0] > 0.6:            
+			velocity.y = JUMP_VELOCITY        
+		# Animación y movimiento si está vivo        
+		if life == true:            
 			if velocity.y < 0:
-				$AnimatedSprite2D.play()
-				set_rotation(deg_to_rad(velocity.y * 0.05))
+				# Animación de subida                
+				$AnimatedSprite2D.play()                
+				set_rotation(deg_to_rad(velocity.y * 0.05))            
 			elif velocity.y > 0: 
-				set_rotation(deg_to_rad(velocity.y * 0.15))
-				$AnimatedSprite2D.stop()
+				# Animación de bajada               
+				set_rotation(deg_to_rad(velocity.y * 0.15))                
+				$AnimatedSprite2D.stop()            
 			else:
-				$AnimatedSprite2D.play()
-				set_rotation(0)
-			
-			move_and_collide(velocity * delta)
+				# Animación de reposo                
+				$AnimatedSprite2D.play()                
+				set_rotation(0)            
+			# Mover y colisionar            
+			move_and_collide(velocity * delta)        
 		else:
-			$AnimatedSprite2D.stop()
+			# Detener animación si no está vivo            
+			$AnimatedSprite2D.stop()    
 	else:
-		if not is_on_floor():
-			velocity.y += gravity * delta * 2
-		else:
-			velocity = Vector2(-200, 0)
+		# Animación de muerte        
+		if not is_on_floor():            
+			velocity.y += gravity * delta * 2        
+		else:            
+			velocity = Vector2(-200, 0)        
 		$AnimatedSprite2D.stop()
 		if animacio == 0 and velocity.y > 0:
+			# Animación de rotación
 			$AnimationPlayer.play('rotacio')
 			animacio += 1
+	
+	# Mover y deslizar
 	move_and_slide()
 
-class NEATNeuron:
-	var output = 0.0
-
-	func __init__():
-		pass
-
-
-class NEATConnection:
-	var from_neuron = 0
-	var to_neuron = 0
-	var weight = 0.0
-
-	func __init__():
-		pass
-
 func inputs():
+	# Entradas para la red neuronal
 	var pos_y_bird :float = get_global_position().y #* (24.0/1585.0) + 3.0
 	var pos_x_obstacle :float = Global.posicio_obstacle_continua[1] #* (3.0/230.0) - (78.0/23.0)
 	var pos_y_obstacle :float = Global.posicio_obstacle_continua[1] #* (3.0/140.0) - (3.0/14.0)
-	
 	return [pos_y_bird, pos_x_obstacle, pos_y_obstacle]
 
 func feedforward(inputs):
+	# Procesamiento de la red neuronal
 	var outputs = []
 	for _i in range(num_inputs):
 		neurons[_i].output = inputs[_i]
@@ -129,9 +148,11 @@ func feedforward(inputs):
 	return outputs
 
 func sigmoid(x):
+	# Función sigmoide
 	return 1 / (1 + exp(-x))
 
 func valor(n):
+	# Valor de una neurona
 	var output :float = 0.0
 	for conection in connections:
 		if conection.to_neuron == n and neurons[conection.from_neuron].output != 0:
@@ -141,21 +162,25 @@ func valor(n):
 	neurons[n].output = output
 
 func mutar():
+	# Mutación de la red neuronal
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	if rng.randf() < 0.1:
 		var n :int = rng.randi_range(0, 4)
 		if n == 0:
+			# Cambio de peso
 			connections[rng.randi_range(0, len(connections))].weight = rng.randf_range(-1, 1)
 		elif n == 1:
 			for connection in connections:
 				if connection.to_neuron == len(neurons) - 1:
 					connection.to_neuron += 1
+			# Agregar neurona
 			connections.insert(len(connections) - 1, NEATConnection.new())
 			connections[len(connections) - 2].from_neuron = rng.randi_range(0, len(neurons) - 2)
 			connections[len(connections) - 2].to_neuron = rng.randi_range(num_inputs, len(neurons) - 1)
 			connections[len(connections) - 2].weight = rng.randf_range(-1.0, 1.0)
 		elif n == 2:
+			# Agregar neurona y conexión
 			neurons.insert(len(neurons) - 1, NEATNeuron.new())
 			connections.insert(len(connections) - 1, NEATConnection.new())
 			while connections[len(connections) - 2].from_neuron == connections[len(connections) - 2].to_neuron:
@@ -167,8 +192,10 @@ func mutar():
 			connections[len(connections) - 2].to_neuron = len(neurons) - 1
 			connections[len(connections) - 2].weight = rng.randf_range(-1.0, 1.0)
 		elif n == 3:
+			# Eliminar conexión
 			connections.remove_at(rng.randi_range(0, len(connections) - 1))
 		elif n == 4:
+			# Eliminar neurona
 			var m := rng.randi_range(num_inputs, len(neurons) - 2)
 			neurons.remove_at(m)
 			for connection in connections:
@@ -180,4 +207,5 @@ func mutar():
 					connection.from_neuron -= 1
 
 func _on_area_2d_body_entered(body):
+	# Muerte del pájaro
 	mort = true
