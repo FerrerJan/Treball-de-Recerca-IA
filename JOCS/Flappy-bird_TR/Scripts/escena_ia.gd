973dec4cfe2b5269a7c0f6ffcf96f14c6fcee30b
Marc Pérez Fusco <07mapefu@gmail.com>
2024-07-15 22:29:16 +0200

    SolucionPromenclaturaDeWindows

--- JOCS/Flappy-bird_TR/Scripts/escena_ia.gd
+++ JOCS/Flappy-bird_TR/Scripts/escena_ia.gd
@@ -237,7 +237,7 @@ func guardar_dades_arxiu():
 	'''
 	
 func desa_arxiu():
-	Global.nom = ('Població:' + str(Global.num_poblacio) + '_Inputs:' + str(Global.inputs) + '_Mutacions:' + str(Global.mutacions)).replace(' ', '')
+	Global.nom = ('Població-' + str(Global.num_poblacio) + '_Inputs-' + str(Global.inputs) + '_Mutacions-' + str(Global.mutacions)).replace(' ', '')
 	
 	var file_name = "res://dades(NO OBRIR DESDE GODOT)/" + Global.nom + '_' + str(Global.num) + '.txt'
 	var PY_file_name = "res://dades(NO OBRIR DESDE GODOT)/" + "PY_" + Global.nom + '_' + str(Global.num) + '.txt'
